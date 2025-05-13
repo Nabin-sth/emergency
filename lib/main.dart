1d73035afc0f15e3b31151eed98294f2e8729f45
Nabin-sth <nabinshrestha120@gmail.com>
2025-05-13 19:08:34 +0545

    socket config

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,10 @@ class SocketService {
     socket = IO.io(baseUrl, <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': true,
+      'reconnection': true,
+      'reconnectionAttempts': 5,
+      'reconnectionDelay': 1000,
+      'reconnectionDelayMax': 5000,
     });
 
     socket.onConnect((_) {
